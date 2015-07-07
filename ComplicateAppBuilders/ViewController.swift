10288dbfed3bae9fc709e9325c0e4e1e4bdef509
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2015-07-07 15:59:36 +0200

    initial commit

---
+++ ComplicateAppBuilders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ComplicateAppBuilders
+//
+//  Created by Vasile Cotovanu on 07/07/15.
+//  Copyright © 2015 Vasile Cotovanu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
